b3a7eaf19ed024229eb76bfad3fed416d31b9400
Tatiana Konischeva <moyzaldi@gmail.com>
2024-11-18 16:22:20 +0300

    init commit

---
+++ conftest.py
@@ -0,0 +1,18 @@
+from selene import browser, be, have
+from selenium import webdriver
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def setting():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+    browser.config.window_height = 2560
+    browser.config.window_width = 1440
+    browser.open('https://google.com/ncr')
+    browser.config.timeout = 20
+
+    yield
+
+    browser.quit()
---
+++ test_google_should_find_selene.py
@@ -0,0 +1,12 @@
+from selene import browser, be, have
+import pytest
+
+
+def test_search_positive():
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_search_negative():
+    browser.element('[name="q"]').should(be.blank).type('sdfsdfsdfsdfsdfasdasassasdaseq23213213').press_enter()
+    browser.element('[id="botstuff"]').should(have.text('did not match any documents'))
